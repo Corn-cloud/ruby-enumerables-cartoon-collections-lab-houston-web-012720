ebe7688d525ae1ab348a7ead7dffb1042bb73189
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-25 02:53:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,11 +15,11 @@ def roll_call_dwarves(dwarves)# code an argument here
 end
 
 
-def summon_captain_planet(# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
    planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
