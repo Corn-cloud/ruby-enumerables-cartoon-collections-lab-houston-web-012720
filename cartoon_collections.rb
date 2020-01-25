8af7209d9141d1d96c68b5d8f3289ec5d0fc38e4
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-25 02:20:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |dwarf, inex|
-    puts "#{index + 1}.#{dwarf}"
+    puts "#{index + 1}. #{dwarf}"
   end
   # Your code here
 end
