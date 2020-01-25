f0b82565592549d71b4faa793d5705083fd4714c
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-25 02:26:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,12 @@
-
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
 
 def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |dwarf, inex|
